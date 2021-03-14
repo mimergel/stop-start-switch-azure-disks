0f4c0b3eac121ca2b007b477c531f7289b630f01
Michael Mergell <mimergel@microsoft.com>
2021-03-14 14:46:40 +0100

    Update Stop-SAP-VM.ps1

--- Stop-SAP-VM.ps1
+++ Stop-SAP-VM.ps1
@@ -85,7 +85,7 @@ workflow Stop-SAP-VM
         Write-Output "Starting VMs"; 
         foreach ($AzureVM in $AzureVMsToHandle) 
         { 
-            Write-Output "Switching all disks to $DiskTypeStarted to improve performance ..."; 
+            Write-Output "Switching all disks to $DiskTypeStarted for optimal performance during operations ..."; 
             $vmResource = Get-AzureRmResource -Name $AzureVM
             $vmDisks = Get-AzureRmDisk -ResourceGroupName $vmResource.ResourceGroupName 
             $vm = Get-AzureRmVM -Name $AzureVM -resourceGroupName $vmResource.ResourceGroupName
